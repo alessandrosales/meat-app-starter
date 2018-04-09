2a44bbed1c13e6f211e9b3dc6bd0fba238760e59
Alessandro Sales <contato@alessandrosales.com.br>
2018-04-09 01:20:26 -0300

    Criação de módulos compatilhados e sobre

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { InputComponent } from './input/input.component'
+import { RadioComponent } from './radio/radio.component'
+import { RatingComponent } from './rating/rating.component'
+
+@NgModule({
+  declarations: [InputComponent, RadioComponent, RatingComponent],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    InputComponent,
+    RadioComponent,
+    RatingComponent,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule {
+
+}
